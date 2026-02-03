754268f5377fb562a985f02bca3609e7c197d149
Dinithi Arunapriya <darunapriya@coveragex.com>
2026-02-03 22:28:08 +0530

    Remove backend to rebuild from scratch

--- backend/src/main/java/com/projectportal/project/model/ProjectStatus.java
+++
@@ -1,9 +0,0 @@
-package com.projectportal.project.model;
-
-public enum ProjectStatus {
-    PLANNED,
-    IN_PROGRESS,
-    ON_HOLD,
-    COMPLETED,
-    AT_RISK
-}
--- backend/src/main/java/com/projectportal/project/repo/ProjectRepository.java
+++
@@ -1,6 +0,0 @@
-package com.projectportal.project.repo;
-
-import com.projectportal.project.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {}
